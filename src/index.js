74726994067c8bf855e75c8c60d72449e2200662
lamtran03072000 <tranhoanglam03072000@gmail.com>
2024-05-17 19:59:16 +0700

    start project react

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// tạo root trên thẻ div #root
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// jsx
+root.render(<div>Hello bc67</div>);
